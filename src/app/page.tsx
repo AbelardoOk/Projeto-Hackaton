0877d8724ec705531657cea9dd348b11ccefadbf
NajuMoraes <135051759+NajuMoraes@users.noreply.github.com>
2024-10-31 11:07:54 -0400

    Criação página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="h-[100vh] w-[100vw]">
       {/* <button className="absolute rounded-lg font-bold px-8 py-2 bg-white text-[#181818] opacity-80 mt-[20rem] ml-[48rem] hover:opacity-100 hover:scale-125 transition-all">Amazônia</button>
       <button className="absolute rounded-lg font-bold px-8 py-2 bg-white text-[#181818] opacity-80 mt-[34rem] ml-[62.5rem] hover:opacity-100 transition-all hover:scale-125">Cerrado</button>
       <button className="absolute rounded-lg font-bold px-8 py-2 bg-[#181818] text-[#f7f7f7] opacity-80 mt-[20rem] ml-[77rem] hover:opacity-100 transition-all hover:scale-125">Caatinga</button>
@@ -14,11 +14,17 @@ export default function Home() {
         <h1 className="text-black font-bold text-4xl">Biomas</h1>
         <Image className="h-[80vh] w-fit" src={"/mapaBiomas.png"} alt="" width={1080} height={1080} />
       </div> */}
-      
+      <div className="flex flex-col justify-center items-center mt-52 font-mono">
+
+        <p className="normal-case text-black font-medium text-7xl bg-slate-300 border rounded-md p-1 w-fit mb-10 ">Energia Sustentável</p>
+        <p className="normal-case text-black font-bold text-2xl bg-slate-300 border rounded-md p-1 w-fit mb-9 ">&#34;Em busca de um futuro renovável&#34;</p>
+
+        <button type="button" className="py-2.5 px-5 text-2x1 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ">Começar</button>
+
+      </div>
 
 
 
-        
     </main>
 
   );
