3dbda4fd306c64f4dd8f0578618b5b14bd35e76c
brenø <136340475+bren0kkj@users.noreply.github.com>
2024-10-31 13:43:58 -0400

    Mapa concluido

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { BotaoHidreletrica } from "@/components/hidreletrica";
 import { BotaoEolica } from "@/components/eolica";
 import { BotaoBiomassa } from "@/components/biomassa";
+import { BotaoGas } from "@/components/gas";
+import { BotaoPetroleo } from "@/components/petroleo";
+import { BotaoCarvao } from "@/components/carvao";
 import { Card } from "@/components/card";
 import { CardEolica } from "@/components/cardEolica";
 import { CardBiomassa } from "@/components/cardBiomassa";
@@ -22,9 +25,12 @@ export default function Home({ searchParams }: SearchParamProps) {
         <BotaoHidreletrica />
         <BotaoEolica />
         <BotaoBiomassa />
+        <BotaoGas />
+        <BotaoPetroleo />
+        <BotaoCarvao />
 
         <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center gap-8">
-            <h1 className="text-black font-bold text-4xl">Biomas do Brasil</h1>
+            <h1 className="text-black font-bold text-4xl">Fontes de Energia Predominantes nos Estados Brasileiros</h1>
             <Image className="h-[80vh] w-fit" src={"/mapaEnergia.png"} alt="" width={1080} height={1080} />
         </div>
 
--- src/components/biomassa.tsx
+++ src/components/biomassa.tsx
@@ -1,11 +1,10 @@
 import { faSpa } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Link from 'next/link'
 
 export function BotaoBiomassa(){
     return(
-        <Link href="/mapa/?biomassa=true" className="flex justify-center items-center absolute p-2 h-20 w-20 rounded-full bg-slate-200 mt-[39rem] ml-[55rem] hover:scale-125 transition-all">    
+        <div className="flex justify-center items-center absolute p-2 h-14   w-14 rounded-full bg-slate-200 mt-[29rem] ml-[46rem] hover:scale-125 transition-all">    
             <FontAwesomeIcon color='#181818' height={48} icon={faSpa} />
-        </Link>
+        </div>
     )
 }
