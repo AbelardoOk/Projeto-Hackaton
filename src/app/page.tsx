e679c06d00341441d9483b380f6a77dfa850923b
brenø <136340475+bren0kkj@users.noreply.github.com>
2024-10-31 14:52:16 -0400

    Gas natural

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,12 @@ export default function Home() {
       
       <div className="flex flex-col justify-center items-center mt-52 font-mono">
 
-        <p className="normal-case text-black font-medium text-7xl bg-slate-300 border rounded-md p-1 w-fit mb-10 ">Energia Sustentável</p>
-        <p className="normal-case text-black font-bold text-2xl bg-slate-300 border rounded-md p-1 w-fit mb-16 ">&#34;Em busca de um futuro renovável&#34;</p>
+        <p className="normal-case text-black font-medium text-7xl bg-slate-300 border rounded-md p-1 w-fit mb-10 ">Energia Renovável</p>
+        <p className="normal-case text-black font-bold text-2xl bg-slate-300 border rounded-md p-1 w-fit mb-16 ">&#34;Em busca de um futuro melhor&#34;</p>
 
         <Link href={"/mapa"} className="py-2.5 px-5 text-3xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ">Começar</Link>
 
       </div>
-
-
 
     </main>
 
