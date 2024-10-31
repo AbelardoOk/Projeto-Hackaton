e563bf18ede25504cdf767ddb32ec199810e41be
NajuMoraes <135051759+NajuMoraes@users.noreply.github.com>
2024-10-31 13:50:59 -0400

    Pequenos detalhes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,13 @@ export default function Home() {
         <h1 className="text-black font-bold text-4xl">Biomas</h1>
         <Image className="h-[80vh] w-fit" src={"/mapaBiomas.png"} alt="" width={1080} height={1080} />
       </div> */}
+      <div className="bg-[url('/')"></div>
       <div className="flex flex-col justify-center items-center mt-52 font-mono">
 
         <p className="normal-case text-black font-medium text-7xl bg-slate-300 border rounded-md p-1 w-fit mb-10 ">Energia Sustentável</p>
-        <p className="normal-case text-black font-bold text-2xl bg-slate-300 border rounded-md p-1 w-fit mb-9 ">&#34;Em busca de um futuro renovável&#34;</p>
+        <p className="normal-case text-black font-bold text-2xl bg-slate-300 border rounded-md p-1 w-fit mb-16 ">&#34;Em busca de um futuro renovável&#34;</p>
 
-        <Link href={"/mapa"} className="py-2.5 px-5 text-2x1 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ">Começar</Link>
+        <Link href={"/mapa"} className="py-2.5 px-5 text-3xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 ">Começar</Link>
 
       </div>
 
