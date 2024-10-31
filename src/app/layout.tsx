6a1d6561cb1cbce5a5667fb9401a63f1e46269ad
AbelardoOk <AbelardoEmilPrivado@gmail.com>
2024-10-31 00:40:31 -0400

    Feat: Atualização no mapa | Criação de botões

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-[#f7f7f7] ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="h-full w-full">
-      <Image className="absolute" src={'/amazonia.png'} alt="" width={480} height={480} />
-      <Image className="absolute ml-[16.61rem] mt-[3.45rem]" src={'/caatinga.png'} alt="" width={480} height={480} />
-      <Image className="absolute ml-28 mt-28" src={'/cerrado.png'} alt="" width={480} height={480} />
-      <Image className="absolute mt-[10.53rem] ml-[11.9rem]" src={'/mataAtlantica.png'} alt="" width={480} height={480} />
-      <Image className="absolute mt-[22.25rem] ml-[4.5rem]" src={'/pampa.png'} alt="" width={480} height={480} />
-      <Image className="absolute mt-[11.65rem] ml-[1.55rem]" src={'/pantanal.png'} alt="" width={480} height={480} />
-    </div>
+    <main>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-white text-[#181818] opacity-80 mt-[20rem] ml-[48rem] hover:opacity-100 hover:scale-125 transition-all">Amazônia</button>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-white text-[#181818] opacity-80 mt-[34rem] ml-[62.5rem] hover:opacity-100 transition-all hover:scale-125">Cerrado</button>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-[#181818] text-[#f7f7f7] opacity-80 mt-[20rem] ml-[77rem] hover:opacity-100 transition-all hover:scale-125">Caatinga</button>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-[#181818] text-[#f7f7f7] opacity-80 mt-[48rem] ml-[64rem] hover:opacity-100 transition-all hover:scale-125">Mata Atlântica</button>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-[#181818] text-[#f7f7f7] opacity-80 mt-[37rem] ml-[47rem] hover:opacity-100 transition-all hover:scale-125">Pantanal</button>
+      <button className="absolute rounded-lg font-bold px-8 py-2 bg-[#181818] text-[#f7f7f7] opacity-80 mt-[54rem] ml-[52rem] hover:opacity-100 transition-all hover:scale-125">Pampa</button>
+
+      <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center gap-8">
+        <h1 className="text-black font-bold text-4xl">Biomas do Brasil</h1>
+        <Image className="h-[80vh] w-fit" src={"/mapaBiomas.png"} alt="" width={1080} height={1080} />
+      </div>
+
+    </main>
+
   );
 }
